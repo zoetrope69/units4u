525e3dcb81b3e9a1905aa0278840e50571b3e575
Zac Colley <hi@zaccolley.com>
2016-01-23 22:06:10 +0000

    Job search function that retrieves job titles and descriptions

--- app/main.js
+++ app/main.js
@@ -3,6 +3,8 @@ require('dotenv').config(); // pull in environment variables from .env
 
 const db = require('./db');
 
+const jobs = require('./jobs');
+
 const query = `
   MATCH (p:Person)-[s:STUDIED]->(unit)
   WHERE "javascript" in p.interests
@@ -19,4 +21,17 @@ db.cypher({ query }, (err, results) => {
 
   console.log(result['unit.title']);
 
+});
+
+jobs.search({
+  amount: 1000,
+  country: 'gb',
+  location: 'London, UK',
+  keywords: ['php', 'developer', 'html', 'css']
+}, (err, results) => {
+  if (err) {
+    return console.log(err);
+  }
+
+  console.log(results);
 });
