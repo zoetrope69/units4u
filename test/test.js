834b9c345d6ef9844f2bd880aff619bd56d3cd06
Zac Colley <hi@zaccolley.com>
2016-01-24 12:38:01 +0000

    Added testing
    Intentional test failure to test

---
+++ test/test.js
@@ -0,0 +1,19 @@
+/* eslint-disable no-magic-numbers, no-undef, no-unused-vars */
+
+const should = require('chai').should();
+const foo = 'bard';
+const beverages = { tea: ['chai', 'matcha', 'oolong'] };
+
+describe('foo', () => {
+  it('should be a string, equal "bar" and be 3 letters long', () => {
+    foo.should.be.a('string');
+    foo.should.equal('bar');
+    foo.should.have.length(3);
+  });
+});
+
+describe('beverages', () => {
+  it('should have 3 values in a lovely tea property', () => {
+    beverages.should.have.property('tea').with.length(3);
+  });
+});
