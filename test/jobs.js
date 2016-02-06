8abed6b51d5dabe3e9f6970155b2c333d2b64abf
Zac Colley <hi@zaccolley.com>
2016-02-06 00:37:11 +0000

    Fixed tests for jobs

--- test/jobs.js
+++ test/jobs.js
@@ -1,75 +1,73 @@
 /* eslint-disable no-magic-numbers, no-undef, no-unused-vars */
+require('dotenv').config(); // pull in environment variables from .env
 
-const jobs = require('../app/jobs');
+const jobs = require('../app/dbMethods/jobsMethods');
+const getJobs = jobs().getJobs;
 const expect = require('chai').expect;
 
-describe('jobs', () => {
-  describe('search', () => {
+describe('getJobs', () => {
+  it('should error if no keywords are sent', () => {
 
-    it('should error if no keywords are sent', () => {
+    // no keywords
+    const options = {
+      location: 'Portsmouth'
+    };
 
-      // no keywords
-      const options = {
-        location: 'Portsmouth'
-      };
-
-      jobs.search(options, (err, jobs) => {
-        expect(err).to.exist;
-        expect(jobs).to.not.exist;
-        expect(err).to.equal('Need at least one keyword');
-      });
-
+    getJobs(options, (err, jobs) => {
+      expect(err).to.exist;
+      expect(jobs).to.not.exist;
+      expect(err).to.equal('Need at least one keyword');
     });
 
-    it('should error if no location is sent', () => {
+  });
 
-      // no location
-      const options = {
-        keywords: ['php', 'js', 'html', 'js']
-      };
+  it('should error if no location is sent', () => {
 
-      jobs.search(options, (err, jobs) => {
-        expect(err).to.exist;
-        expect(jobs).to.not.exist;
-        expect(err).to.equal('Need to set a location');
-      });
+    // no location
+    const options = {
+      keywords: ['php', 'js', 'html', 'js']
+    };
 
+    getJobs(options, (err, jobs) => {
+      expect(err).to.exist;
+      expect(jobs).to.not.exist;
+      expect(err).to.equal('Need to set a location');
     });
 
-    it('should return an array of jobs (specified by an amount)', () => {
+  });
 
-      // no location
-      const options = {
-        amount: 10,
-        country: 'gb',
-        location: 'London, UK',
-        keywords: ['php', 'js', 'html', 'js']
-      };
+  it('should return an array of jobs (specified by an amount)', () => {
 
-      jobs.search(options, (err, jobs) => {
-        expect(err).to.not.exist;
-        expect(jobs).to.exist;
-        expect(jobs).to.be.an('array');
-        expect(jobs).to.be.at.most(10);
-      });
+    // no location
+    const options = {
+      amount: 10,
+      country: 'gb',
+      location: 'London, UK',
+      keywords: ['php', 'js', 'html', 'js']
+    };
 
+    getJobs(options, (err, jobs) => {
+      expect(err).to.not.exist;
+      expect(jobs).to.exist;
+      expect(jobs).to.be.an('array');
+      expect(jobs).to.be.at.most(10);
     });
 
-    it('should return an empty array if no jobs', () => {
+  });
 
-      // no location
-      const options = {
-        location: 'Fake Place Name, UK',
-        keywords: ['cool']
-      };
+  it('should return an empty array if no jobs', () => {
 
-      jobs.search(options, (err, jobs) => {
-        expect(err).to.not.exist;
-        expect(jobs).to.exist;
-        expect(jobs).to.be.an('array');
-        expect(jobs).to.be.empty;
-      });
+    // no location
+    const options = {
+      location: 'Fake Place Name, UK',
+      keywords: ['cool']
+    };
 
+    getJobs(options, (err, jobs) => {
+      expect(err).to.not.exist;
+      expect(jobs).to.exist;
+      expect(jobs).to.be.an('array');
+      expect(jobs).to.be.empty;
     });
 
   });
